3e462c8cfdb6a30d5cf21d168d9f3e107f8fa07a
exe <ex177@hotmail.com>
2019-09-29 18:47:58 +0200

    add missing sound in constants

--- src/pages/constants.js
+++ src/pages/constants.js
@@ -6,10 +6,11 @@ import sound1 from "../audio/congratulations/1.mp3";
 import sound2 from "../audio/congratulations/2.mp3";
 import sound3 from "../audio/congratulations/3.mp3";
 import sound4 from "../audio/congratulations/4.mp3";
+import sound5 from "../audio/congratulations/5.mp3";
 
 const shapes = ["S", "R", "T", "C"];
 const colors = ["#F62817", "#1589FF", "#4AA02C"];
-const congratulationsSounds = [sound1, sound2, sound3, sound4];
+const congratulationsSounds = [sound1, sound2, sound3, sound4, sound5];
 
 const colorsMap = {
   "#F62817": "R",
